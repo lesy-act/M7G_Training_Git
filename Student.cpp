12c85303ac275f3d24d3e8577bccaed1c0d11f40
lesy-act <=>
2022-10-09 00:11:00 +0700

    create get set function for Student class

--- Student.cpp
+++ Student.cpp
@@ -3,7 +3,23 @@ Student::Student()
 {
 
 }
-Student::Student(std::string School, double Gpa):m_School(School),m_Gpa(Gpa)
+Student::Student(std::string &School, double &Gpa):m_School(School),m_Gpa(Gpa)
 {
 
+}
+void Student::setSchool(const std::string &nameSchool)
+{
+    m_School = nameSchool;
+}
+void Student::setGpa(const double &Gpa)
+{
+    m_Gpa = Gpa;
+}
+std::string Student::getSchool()const
+{
+    return m_School;
+}
+double Student::getGPA()const
+{
+    return m_Gpa;
 }
